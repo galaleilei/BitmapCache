4ba22b552edbfe8e475c57f6fc570216a28e36e7
ga <leimin_398056693@qq.com>
2016-08-17 16:16:59 +0800

    练习增加代码

--- app/src/main/java/qf/com/bitmapcache/Bb.java
+++ app/src/main/java/qf/com/bitmapcache/Bb.java
@@ -4,4 +4,14 @@ package qf.com.bitmapcache;
  * Created by ga on 2016/8/17.
  */
 public class Bb {
+
+    int a=1;
+    int ss=1;
+    int asss=1;
+    int assss=1;
+    int aqq=1;
+    int aqaa=1;
+    int aq=1;
+    int af=1;
+    int ah=1;
 }
