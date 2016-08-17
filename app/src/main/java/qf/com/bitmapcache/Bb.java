be49e7e8b63476f58ee8f2f7f877ee20d0570269
ga <leimin_398056693@qq.com>
2016-08-17 16:18:01 +0800

    增加的垃圾文件2222111

--- app/src/main/java/qf/com/bitmapcache/Bb.java
+++ app/src/main/java/qf/com/bitmapcache/Bb.java
@@ -4,4 +4,9 @@ package qf.com.bitmapcache;
  * Created by ga on 2016/8/17.
  */
 public class Bb {
+
+    String abc="http://10.36.137.42:8080/androidoo/q";
+    String a="ccd11";
+    String x="ccd12";
+
 }
